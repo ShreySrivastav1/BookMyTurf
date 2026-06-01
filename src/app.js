4244c641d598d3325c2ad31bbe94dd3c006ddf7e
Shrey Srivastav <shreysrivastav134678@gmail.com>
2026-06-01 13:10:42 +0530

    added cors for prod

--- src/app.js
+++ src/app.js
@@ -13,14 +13,14 @@ const paymentRouter = require("./routes/paymentRou");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-    cors(
-        {
-            origin:"http://localhost:5173",
-            credentials: true
-        }
-    )
-);
+
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://13.127.251.76"
+    ],
+    credentials: true
+}));
 
 app.use("/",authRouter);
 app.use("/",profileRouter);
@@ -32,7 +32,7 @@ app.use("/",paymentRouter);
 connectDb().then(() => {
     console.log("Successfully connected to Database");
     app.listen(process.env.PORT, () => {
-        console.log("Server is listening on port");
+        console.log(`Server running on port ${process.env.PORT}`);
 })
 }).catch((err) => {
     console.log("Unable to connect to DB");
